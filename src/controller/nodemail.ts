d057a5e08db04ce8f9de1260b012a5c7b109cd36
Samuel Dias <samuel.sdias@hotmail.com>
2021-03-10 16:54:10 -0300

    Template mail

--- src/controller/nodemail.ts
+++ src/controller/nodemail.ts
@@ -1,20 +1,44 @@
+var hbs = require('handlebars');
+var handlebars = require('handlebars');
+var fs = require('fs');
+const path = require('path'); 
+//var fooJson = require('path/to/foo.json');
 var nodemailer = require('nodemailer');
- 
-module.exports = (nome: string, email: string, assunto: string, mensagem: string) => {
 
+module.exports = async (nome: string, email: string, assunto: string, mensagem: string) => {
     const smtpTransport = nodemailer.createTransport({
-        service: 'Gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
         auth: {
             user: 'samukdias97@gmail.com',
             pass: 'samuk1415'
-        }
+        },
     })
     
+    var usuario = {
+        remetente: `${nome} <${email}>`,
+        destinatario: `Samuel de Sousa Dias <samukdias97@gmail.com>`,
+        assunto: assunto,
+        mensagem: mensagem,
+    }
+    const path1 = path.resolve("src/controller", "contact.hbs")
+    const templateFileContent = await fs.promises.readFile(path1, {
+        encoding: 'utf-8'
+    })
+    var parseTemplate = handlebars.compile(templateFileContent);
+
+    var html = parseTemplate(usuario);
+
     const mail = {
-        from: email,
+        from: {
+            name: `${nome} - ${email}`,
+            address: email,
+        },
         to: 'samukdias97@gmail.com',
-        subject: `Nome: ${nome} - Email: ${email} - Assunto: ${assunto}`,
+        subject: `${assunto} - My Portfolio`,
         text: mensagem,
+        html: html,
     }
     
     return new Promise((resolve, reject) => {
---
+++ src/shared/index.ts
@@ -0,0 +1,9 @@
+import { container } from 'tsyringe';
+
+import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
+import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
+
+container.registerSingleton<IMailTemplateProvider>(
+  'MailTemplateProvider',
+  HandlebarsMailTemplateProvider,
+)

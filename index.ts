775233ff19940358bc6e0d91486f32e173e9e94c
Samuel Dias <samuel.sdias@hotmail.com>
2021-03-12 20:55:52 -0300

    Added process.env.PORT to index.ts

--- index.ts
+++ index.ts
@@ -9,6 +9,6 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
-app.listen(3000, ()=>{
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server up!');
 })
